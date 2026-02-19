e45d1d9c6c005b3f5aebc95c84b0d15a185e52c3
exadmin <smirnovi.qubership@gmail.com>
2026-02-19 12:17:03 +0300

    feat: Adding pre-commit configuration & scripts

---
+++ pre-commit-script/run-checks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+'''':
+for interpreter in python3 python2 python
+do
+    which $interpreter >/dev/null 2>&1 && exec $interpreter "$0" "$@"
+done
+echo "$0: No python could be found" >&2
+exit 1
+# '''
+
+import os
+import sys
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+
+def main():
+    # cfg = os.path.join(HERE, 'orghooks.yaml')
+    # cmd = ['pre-commit', 'run', '--config', cfg, '--files'] + sys.argv[1:]
+    # os.execvp(cmd[0], cmd)
+    print("Hello World!")
+
+if __name__ == '__main__':
+    exit(main())
