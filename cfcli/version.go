fab49f27c98271f06e5265f94a0ae06abaa6a34e
exadmin <smirnovi.qubership@gmail.com>
2026-09-01 19:14:46 +0300

    chore(release): bump version to 2.1.2

--- cfcli/version.go
+++ cfcli/version.go
@@ -5,9 +5,9 @@ import (
 	"io"
 )
 
-const appVersion = "2.1.1"
+const appVersion = "2.1.2"
 
 func writeAppVersion(output io.Writer) error {
-	_, err := fmt.Fprintf(output, "TXT: CyberFerret CLI, version %s\n", appVersion)
+	_, err := fmt.Fprintf(output, "TEXT: CyberFerret CLI, version %s\n", appVersion)
 	return err
 }
