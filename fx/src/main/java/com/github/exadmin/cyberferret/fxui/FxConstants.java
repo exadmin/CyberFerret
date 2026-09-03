72bb9d7c81d86aa4558c7fc8c311adf8f24bc96f
exadmin <smirnovi.qubership@gmail.com>
2026-09-03 13:19:34 +0300

    refactor(fx): prevent FxConstants instantiation

--- fx/src/main/java/com/github/exadmin/cyberferret/fxui/FxConstants.java
+++ fx/src/main/java/com/github/exadmin/cyberferret/fxui/FxConstants.java
@@ -1,6 +1,12 @@
 package com.github.exadmin.cyberferret.fxui;
 
-public class FxConstants {
+public final class FxConstants {
     public static final int DEFAULT_BUTTON_WIDTH = 120;
     public static final int DEFAULT_LABEL_WIDTH = 120;
+
+    /**
+     * Prevents instantiation of this constants holder.
+     */
+    private FxConstants() {
+    }
 }
